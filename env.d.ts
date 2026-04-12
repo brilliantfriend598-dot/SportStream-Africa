665f6725e0fd90c65621be04bc578dc2ba8096a3
Brilliant Mandivenga <brilliantfriend598@gmail.com>
2026-04-12 21:03:09 +0200

    env2

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    EXPO_PUBLIC_FOOTBALL_API_BASE_URL: string;
+    EXPO_PUBLIC_FOOTBALL_API_KEY: string;
+    EXPO_PUBLIC_DEFAULT_SEASON: string;
+    EXPO_PUBLIC_DEFAULT_TIMEZONE: string;
+  }
+}
